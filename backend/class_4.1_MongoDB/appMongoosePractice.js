6ee56ae8a2772fa2088a48b4c54ac683488e9b28
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-09-24 20:32:43 +0530

    validation in databse schema and basic api validation in crud opeartion

---
+++ backend/class_4.1_MongoDB/appMongoosePractice.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const UserData = require("./models/ValidationDB.js");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/info", async (req, res) => {
+
+    try{
+         const result = await UserData.find();
+         res.send(result);
+    }catch(err){
+        res.send("Error from get router : " + err.message)
+    }
+   
+});
+
+app.get("/info/:id", async (req, res) => {
+
+    try{
+        
+        const data = await UserData.findById(req.params.id);
+        res.send(data);
+
+    }catch(err){
+        res.send("error from get id route : " + err.message);
+    }
+})
+
+app.post("/register", async(req, res) => {
+
+    // api validation
+    const mandatoryFields = ["FirstName", "email", "age", "password"];
+    const isAllowed = Object.keys(req.body).every((keys) => mandatoryFields.includes(keys));
+    if(!isAllowed)
+        throw new Error("Fields are missing ")
+
+
+
+    try{
+
+        await UserData.create(req.body);
+        res.status(200).send("User registered sucessfully")
+    }catch(err){
+        console.log(err.message + "this error is occuring in post route")
+    }
+});
+
+app.patch("/update", async(req, res) => {
+
+    try{
+
+        const {_id, ...update} = req.body;
+        await UserData.findByIdAndUpdate(_id, update, { runValidators: true});
+        res.send("update sucessfully");
+
+
+    }catch(err){
+        res.send("Error from patch route : " + err.message);
+    }
+})
+
+app.delete("/delete/:id", async (req, res) => {
+
+    try{
+
+        await UserData.findByIdAndDelete(req.params.id);
+        res.send("deleted sucessfully");
+    }catch(err){
+        res.send("Error from delete route : " + err.message);
+
+    }
+})
+
+
+// app.get("/get", async (req, res) => {
+//     try {
+//         const users = await User.find();
+//         res.json(users);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("Error fetching users");
+//     }
+// });
+
+// app.post("/post", async(req, res) => {
+
+//     // const ans = new User(req.body);
+//     // await ans.save();
+
+//     try{
+//     await User.create(req.body);
+//     res.send("Successfully created user");
+//     }catch(err){
+//         res.status(500).send(err + " error occured");
+//     }
+// });
+
+
+// app.put("/put", async(req, res) => {
+
+//     // const resp = await User.updateOne({name: "Harshit"}, {name: "harshitttttt"});
+//        const resp = await User.updateOne({name: "harshitttttt"}, {email: "harshitttt888888@gmail.com"});
+//     res.status(200).send(resp + "updated sucessfully")
+// })
+
+// app.delete("/delete", async(req, res) => {
+//     await User.deleteOne({name: "nia"});
+//     res.status(200).send("Deleted sucessfully")
+// })
+
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        console.log("Database connected successfully");
+        
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+
+        // mongo query
+        // const resp = await User.find();
+        // console.log(resp);
+
+    } catch (error) {
+        console.error("Failed to start server:", error);
+        
+    }
+};
+
+startServer();
---
+++ backend/class_4.1_MongoDB/models/ValidationDB.js
@@ -0,0 +1,50 @@
+const mongoose =  require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required : true
+    },
+    lastName:{
+        type: String
+    },
+    age:{
+        type: Number,
+        min: 16,   // validation
+        max: 45
+    },
+    gender:{
+        type: String,
+        // enum : ["male", "female", "others"],
+
+        validate(value){
+            if(!["male", "female", "others"].includes(value))
+                throw new Error("Invalid Gender");
+        }
+    },
+    email:{
+        type: String,
+         required : true,
+         unique : true,
+         trim : true,
+         lowercase: true,
+         immutable: true
+    },
+    password:{
+        type: String,
+         required : true
+    },
+    image:{
+        type: String,
+        default : "This is the default image"
+    },
+
+
+
+},
+{timestamps : true}
+)
+
+
+const UserData = mongoose.model('user', userSchema);
+module.exports = UserData;
